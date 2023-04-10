064f508fe7d54a79f2a28f0c8ed0950e18e190fd
@Sahak33 <s.iskanian33@gmail.com>
2023-04-10 17:23:11 +0400

    add Product details page

--- src/pages/productDetails/index.tsx
+++ src/pages/productDetails/index.tsx
@@ -1,11 +1,69 @@
-import './ProductDetails.scss'
+import "./ProductDetails.scss";
+import { product } from "./data";
+import { useNavigate } from "react-router-dom";
+import { ReactComponent as ArrowLeftIcon } from "assets/arrowLeft.svg";
+import { formatCurrency } from "helpers/currency";
+import { formatDate } from "helpers/date";
 
 const ProductDetails = () => {
-    return (
-        <div>
-            Product Details page
-        </div>
-    )
-}
+  const navigate = useNavigate();
 
-export default ProductDetails
+  const { authors, categories, published, longDescription, thumbnailUrl, title, status } = product;
+  const date = new Date(published?.$date);
+
+  const navigateToBack = () => navigate("/");
+
+  return (
+    <div className="productDetails">
+      <div className="productDetails_back" onClick={navigateToBack}>
+        <ArrowLeftIcon className="productDetails_back_icon" />
+        <p className="productDetails_back_text">Back</p>
+      </div>
+      <div className="productDetails_container">
+        <div className="productDetails_container_img">
+          <img src={thumbnailUrl} alt="book" />
+        </div>
+        <div className="productDetails_container_info">
+          <div>
+            <span className="title">Title:</span>
+            <span className="value">{title}</span>
+          </div>
+          <div>
+            <span className="title">Authors:</span>
+            <span className="value">
+              {authors?.map((author) => (
+                <span key={author}>{author}</span>
+              ))}
+            </span>
+          </div>
+          <div>
+            <span className="title">Categories:</span>
+            <span className="value">
+              {categories?.map((categori) => (
+                <span key={categori}>{categori}</span>
+              ))}
+            </span>
+          </div>
+          <div>
+            <span className="title">Price:</span>
+            <span className="value">{formatCurrency(published?.price, published?.currency)}</span>
+          </div>
+          <div>
+            <span className="title">Date:</span>
+            <span className="value">{formatDate(date)}</span>
+          </div>
+          <div>
+            <span className="title">Status:</span>
+            <span className="value">{status}</span>
+          </div>
+        </div>
+      </div>
+      <div className="productDetails_description">
+        <p className="title">Description</p>
+        <p className="value">{longDescription}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
