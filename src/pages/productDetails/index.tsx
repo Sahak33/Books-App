43630d7ae4bd57d8f5064f2d8df9cc1af515eaf5
@Sahak33 <s.iskanian33@gmail.com>
2023-04-10 10:40:46 +0400

    add Product Details page

---
+++ src/pages/productDetails/index.tsx
@@ -0,0 +1,11 @@
+import './ProductDetails.scss'
+
+const ProductDetails = () => {
+    return (
+        <div>
+            Product Details page
+        </div>
+    )
+}
+
+export default ProductDetails
