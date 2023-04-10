be901dbc303ffee5dcf75a9dee6f3fe5e17e6319
@Sahak33 <s.iskanian33@gmail.com>
2023-04-10 12:01:03 +0400

    update Search component

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,11 +1,31 @@
-import './Search.scss'
+import { ChangeEvent, FC, useState } from "react";
+import { ReactComponent as SearchIcon } from "assets/search.svg";
+import { ReactComponent as CloseIcon } from "assets/close.svg";
+import "./Search.scss";
 
-const Search = () => {
-    return (
-        <div>
-            Search
-        </div>
-    )
-}
+const Search: FC = () => {
+  const [search, setSearch] = useState<string>("");
+
+  const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => setSearch(e.target.value);
+  const handleClearSearch = () => setSearch("");
+
+  return (
+    <div className="search">
+      <label htmlFor="search" className="search_icon">
+        <SearchIcon className="icon" />
+      </label>
+      <input
+        autoComplete="off"
+        className="search_input"
+        id="search"
+        onChange={handleOnChange}
+        placeholder="Search books..."
+        type="text"
+        value={search}
+      />
+      {search.length > 0 && <CloseIcon className="search_clear_icon" onClick={handleClearSearch} />}
+    </div>
+  );
+};
 
 export default Search;
