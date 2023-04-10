6a53cc1d12209d1682179b439d1b0e82bbce5998
@Sahak33 <s.iskanian33@gmail.com>
2023-04-10 15:11:09 +0400

    add Select component

---
+++ src/components/inputs/select/index.tsx
@@ -0,0 +1,16 @@
+import "./Select.scss";
+
+const Select = () => {
+  return (
+    <select className="select">
+      <option>Open Source</option>
+      <option>Mobile</option>
+      <option>Java</option>
+      <option>Software Engineering</option>
+      <option>Internet</option>
+      <option>Web Development</option>
+    </select>
+  );
+};
+
+export default Select;
