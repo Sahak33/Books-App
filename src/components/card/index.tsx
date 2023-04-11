19349cf287d3174194dea02fc3ddbd5765ca4df6
@Sahak33 <s.iskanian33@gmail.com>
2023-04-11 13:51:19 +0400

    add dynamic Link

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -8,20 +8,21 @@ interface ICard {
   authors: string[];
   categories: string[];
   currency: string;
+  id: number;
   price: number;
   shortDescription: string;
   thumbnailUrl: string;
   title: string;
 }
 
-const Card: FC<ICard> = ({ authors, categories, currency, price, shortDescription, thumbnailUrl, title }) => {
+const Card: FC<ICard> = ({ authors, categories, currency, id, price, shortDescription, thumbnailUrl, title }) => {
   return (
     <div className="card">
       <div className="card_main">
-        <Link to="/catalog/5" className="card_main_img">
+        <Link to={`catalog/${id}`} className="card_main_img">
           <img src={thumbnailUrl} alt="book" />
         </Link>
-        <Link to="/catalog/5" className="card_main_title">
+        <Link to={`catalog/${id}`} className="card_main_title">
           {title}
         </Link>
         {authors?.length > 0 && (
