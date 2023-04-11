9527e16e9765456022f1b1128f36de40c505965d
@Sahak33 <s.iskanian33@gmail.com>
2023-04-11 17:39:38 +0400

    fix number input

--- src/components/inputs/numberInput/index.tsx
+++ src/components/inputs/numberInput/index.tsx
@@ -1,22 +1,13 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import { UseFormRegisterReturn } from "react-hook-form";
-import { ReactComponent as CloseIcon } from "assets/close.svg";
 import "./NumberInput.scss";
 
 interface INumberInput {
   register: UseFormRegisterReturn;
 }
 
-const NumberInput: FC<INumberInput> = ({ register }) => {
-  const [value, setValue] = useState<string>("");
-  const handleClearSearch = () => setValue("");
-
-  return (
-    <div className="numberInput">
-      <input autoComplete="off" className="numberInput_input" placeholder="Search price..." type="number" {...register} />
-      {value.length > 0 && <CloseIcon className="numberInput_clear_icon" onClick={handleClearSearch} />}
-    </div>
-  );
-};
+const NumberInput: FC<INumberInput> = ({ register }) => (
+  <input autoComplete="off" className="numberInput" placeholder="Search price..." type="number" {...register} />
+);
 
 export default NumberInput;
