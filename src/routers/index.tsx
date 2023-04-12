aef9a9d63d9c7b342863773d5087ddefd27907a9
@Sahak33 <s.iskanian33@gmail.com>
2023-04-12 12:39:44 +0400

    fix routers

---
+++ src/routers/index.tsx
@@ -0,0 +1,12 @@
+import Catalog from "pages/catalog";
+import ProductDetails from "pages/productDetails";
+import { Routes, Route } from "react-router-dom";
+
+const Routers = () => (
+  <Routes>
+    <Route path="/" element={<Catalog />} />
+    <Route path="/catalog/:id" element={<ProductDetails />} />
+  </Routes>
+);
+
+export default Routers;
